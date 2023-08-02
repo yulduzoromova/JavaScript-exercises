840e71775ece0cc6b34a3acbdbd703051172fdc5
yulduzoromova <yulduzoromova1993@gmail.com>
2023-08-02 16:16:37 +0300

    Switch | For loop

---
+++ 5-dars/script.js
@@ -0,0 +1,49 @@
+// 1.
+// browser = ''
+// switch (browser) {
+//   case 'Edge':
+//     alert("You've got the Edge!");
+//     break;
+
+//     case 'Chrome':
+//       case 'Firefox':
+//         case 'Safari':
+//           case 'Opera':
+//             alert( 'Okay we support these browser too');
+//             break;
+
+//             default:
+//               alert('We hope that this page looks ok!');
+// }
+
+//=======================================
+// 2.
+// let a = +prompt('a?', '');
+
+// if (a == 0) {
+//   alert( 0 );
+// }
+// if (a == 1) {
+//   alert( 1 );
+// }
+// if (a == 2 || a == 3) {
+//   alert( 2, 3);           // alertda 1-javob 2ni chiqarayapti har ikkala holatda ham
+// }                         // chunki alert faqat bitta argument kutadi, shuning uchun qolganlari ignore bo'lib ketadi
+
+
+// 3. Output prime numbers 
+// 100gacha bo'lgan faqat tub sonlar
+
+// for(let num = 2; num <= 100; num++){
+//   prime = true;
+//   for(let i = 2; i < num; i++){
+//     if(num % i == 0){
+//       prime = false;
+//       break;
+//     }
+//   }
+//   if (prime == true) {
+//     console.log(num);
+//   }
+// }
+
