56fbb150d505499163e3e1ed996a05a6921c0398
yulduzoromova <yulduzoromova1993@gmail.com>
2023-08-24 16:19:20 +0300

    Numbers

--- 9-dars/script.js
+++ 9-dars/script.js
@@ -23,7 +23,7 @@ function count() {
 count()
 
 
-// 2. Berilgan sonlar ichidan eng katta va eng kichik sonlarning ayirmasini toping. Manfiy son chiqmasin.
+// 2. Berilgan sonlar ichidan eng katta va eng kichik sonlarning ayirmasini toping. Musbat son chiqmasin.
 
 // let min = Math.min(2, 4, 5, 77, 6, 13, 34, 25)
 // let max = Math.max(2, 4, 5, 77, 6, 13, 34, 25)
@@ -33,14 +33,14 @@ count()
 
 
 // 3. Math ning trunc vs floor vs ceil, paresInt lar bilan farqini tushintirib bering.
-// Math.floor sonni kasr qismini inkor qilin butun qismini chiqaradi masalan, 2.9 bo'lsa 2 qaytaradi. Math.ceil sonni kasr qismi bor bo'lsa undan keyingi sonni chiqaradi ya'ni 2.1 bo'lsa 3 chiqaradi. Math.trunc huddi Math.floorga o'xshaydi ln bitta farqi Math.floor sonni kasr qismi bn qo'shib sonni butun qismini chiqarsa a Math.trunc esa sonni kasr qismini umuman inkor qiladi floorga o'xshab yaxlitlab olamaydi. Manfiy sonlarda esa floor o'zidan past songa qarab yaxlitlasa masalan -10.999 = -11, a trunc manfiy sonda ham kasr qismini inkor qiladi ya'ni -10.999 bo'lsa -10 chiqaradi.
+// Math.floor sonni kasr qismini inkor qilib butun qismini chiqaradi masalan, 2.9 bo'lsa 2 qaytaradi. Math.ceil sonni kasr qismi bor bo'lsa undan keyingi sonni chiqaradi ya'ni 2.1 bo'lsa 3 chiqaradi. Math.trunc huddi Math.floorga o'xshaydi ln bitta farqi Math.floor sonni kasr qismi bn qo'shib sonni butun qismini chiqarsa a Math.trunc esa sonni kasr qismini umuman inkor qiladi floorga o'xshab yaxlitlab olmaydi. Manfiy sonlarda esa floor o'zidan past songa qarab yaxlitlasa masalan -10.999 = -11, a trunc manfiy sonda ham kasr qismini inkor qiladi ya'ni -10.999 bo'lsa -10 chiqaradi.
 
 
 // 4. randomIntiger(min, max) function yozilsin. min max oralig'idagi random raqam chiqsin
 // ex: min = 1, max = 50
 // output: 1dan 50gacha bo'lgan ixtiyoriy raqam 
 
-// function getRandomInt(min, max) {
-//   return Math.floor(Math.random() * (max - min + 1)) + min;
-// }
-//  console.log(getRandomInt(1, 50));
+function getRandomInt(min, max) {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+ console.log(getRandomInt(1, 50));
